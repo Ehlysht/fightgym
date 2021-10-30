5454f10248f33206b8e383fd618f8045352b9c6a
Tomash Ehlysh <ehlysht@gmail.com>
2021-10-30 23:52:51 +0300

    First page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.gallerty__slick').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        rows: 2,
+        centerPadding: '240px',
+		infinite: true,
+        arrows: false,
+        dots: false,
+        centerMode: true,
+    })
+});
